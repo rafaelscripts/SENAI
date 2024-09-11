86c867872a5b872988b0c1c6e8fd0c06c28371e3
Rafael Rodrigues <rrdoidao84@gmail.com>
2024-09-11 17:31:17 -0300

    Add files via upload

---
+++ css/script.js
@@ -0,0 +1,8 @@
+// Menu toggle script
+document.querySelector('.btn-menu').addEventListener('click', function() {
+    document.querySelector('.menu').style.display = 'block';
+});
+
+document.querySelector('.btn-close').addEventListener('click', function() {
+    document.querySelector('.menu').style.display = 'none';
+});
